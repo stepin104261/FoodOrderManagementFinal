8ef0ea8280834a0b3b217e4073413fb04317046f
stepin104261 <69055585+stepin104261@users.noreply.github.com>
2020-10-09 02:48:50 +0530

    Add files via upload

---
+++ 03_Implementation/inc/food.h
@@ -0,0 +1,41 @@
+#ifndef FOOD_H_INCLUDED
+#define FOOD_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+//#include<conio.h>
+#include<string.h>
+//#include<windows.h>
+#include<time.h>
+
+/// Update & Insert Function
+void insertfirst(int data, char foodname[25], int quantity, float price);
+void insertmid(int pos, int data, char foodname[25], int quantity, float price);
+void insertend(int data, char foodname[25], int quantity, float price);
+void updatefood(int udata, int uquantity);
+
+/// Display Function
+void foodlist();
+void order_view(int order, int quantity, int or_no);
+void main_menu();
+
+///Delete & Count Function
+int deletefood(int serial);
+int countitem();
+
+///extra design Function
+void cls();
+void echo(char print[]);
+void br(int line);
+void pre(int tab);
+void span(int space);
+//void ccolor(int clr);
+void pwelcome();
+void loadingbar();
+void middle1(void);
+void middtab1(void);
+void backuploader(void);
+
+//typedef struct Node node ;
+
+#endif // FOOD_H_INCLUDED
---
+++ 03_Implementation/src/food.c
@@ -0,0 +1,60 @@
+#include <food.h>
+
+struct Node{
+
+	char foodname[50];
+	int quantity;
+	float price;
+	int data;
+	struct Node *next;
+
+};
+
+int deletefood(int serial){
+
+	node *temp;
+	temp=(node *)malloc(sizeof(node));
+
+	temp = list;
+
+	if(temp->data != serial){
+
+		while(temp->next->data != serial){
+			temp = temp->next;
+		}
+
+		if(temp->next->data == serial){
+
+			temp->next = temp->next->next;
+			cls();
+			printf("\n\n\n\n\t\t\tDeleting Item %d ",serial);
+			for(int cs=0;cs<4;cs++)
+			{
+				printf(" .");Sleep(400);
+			}
+
+			printf("\n\n\n\n\t\t\tDeleted Successfully \n"); //Sleep(500);
+
+		}
+		else{
+			printf("\n\n\n\n\t\t\tFood Item Not Found\n"); //Sleep(500);
+		}
+
+		head = temp ;
+
+	}
+	else{
+
+		temp = temp->next;
+		cls();
+		printf("\n\n\n\n\t\t\tDeleting Item %d ",serial);
+		for(int cs=0;cs<4;cs++){printf(" .");//Sleep(400);}
+
+		printf("\n\n\n\n\t\t\tDeleted Successfully \n"); //Sleep(500);
+
+		head = temp ;
+
+		list=head;
+	}
+	return 1;
+}
