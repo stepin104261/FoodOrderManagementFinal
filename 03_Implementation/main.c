fe429ed4873396ef58ca0c73cac0f350917078ae
stepin104261 <69055585+stepin104261@users.noreply.github.com>
2020-10-09 03:12:35 +0530

    Update main.c

--- 03_Implementation/main.c
+++ 03_Implementation/main.c
@@ -674,56 +674,6 @@ void insertmid(int pos, int data, char foodname[25], int quantity, float price){
 	//    free(temp);
 }
 
-/*int deletefood(int serial){
-
-	node *temp;
-	temp=(node *)malloc(sizeof(node));
-
-	temp = list;
-
-
-	if(temp->data != serial){
-
-		while(temp->next->data != serial){
-			temp = temp->next;
-		}
-
-		if(temp->next->data == serial){
-
-			temp->next = temp->next->next;
-			cls();
-			printf("\n\n\n\n\t\t\tDeleting Item %d ",serial);for(int cs=0;cs<4;cs++)
-			{
-			    printf(" ."); //Sleep(400);}
-
-			printf("\n\n\n\n\t\t\tDeleted Successfully \n");
-
-		}
-	}
-		else{
-			printf("\n\n\n\n\t\t\tFood Item Not Found\n"); //Sleep(500);
-		}
-
-		head = temp ;
-
-	}
-	else{
-
-		temp = temp->next;
-		cls();
-		printf("\n\n\n\n\t\t\tDeleting Item %d ",serial);for(int cs=0;cs<4;cs++)
-		{
-		    printf(" ."); //Sleep(400);}
-
-		printf("\n\n\n\n\t\t\tDeleted Successfully \n"); //Sleep(500);
-
-		head = temp ;
-
-		list=head;
-	}
-	return 1;
-}
-}*/
 void updatefood(int udata, int uquantity){
 
 	node *temp;
@@ -923,9 +873,9 @@ void loadingbar(void){
 
 		}
 		//Sleep(100);
-		if(i==90 || i==50 || i==96 || i==83){
+		//if(i==90 || i==50 || i==96 || i==83){
 			//Sleep(100);
-		}
+		//}
 
 	}
 
